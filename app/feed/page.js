7f75bedc944649aa51ddceacb5b926e0ceea759d
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-15 00:23:45 +0200

    meta data optimizatio

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,17 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// you can generate a dynamic metadata by exporting a function called generateMetadata that
+// allow you to generate dynamic metadata
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+  const postsNumber = posts.length;
+  return {
+    title: `View all of our ${postsNumber} posts `,
+    description: 'These are all of our posts',
+  };
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
--- app/page.js
+++ app/page.js
@@ -3,6 +3,15 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// the meta data can be set for the seo it must be exported and named metadata
+//  it has title property and it shows as the page title in the browser and description.
+// the description is what will be shown under the title in google search
+
+export const metadata = {
+  title: 'latest posts',
+  description: 'this is the latest posts has been set',
+};
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
@@ -14,9 +23,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
